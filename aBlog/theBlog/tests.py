f936af023603c8876a9cdc25856965ad3843a86e
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-10-04 16:55:19 +0100

    fixed tests

--- aBlog/theBlog/tests.py
+++ aBlog/theBlog/tests.py
@@ -7,12 +7,13 @@ from .models import Category, Comment, Profile, Post
 class PostTestCase(TestCase):
 
     def setUp(self):
-        user1 = User.obects.create(username='default', password='password',
+        user1 = User.objects.create(username='default', password='password',
         email='default@email.com')
-        user2 = User.obects.create(username='default1', password='password1',
+        user2 = User.objects.create(username='default1', password='password1',
         email='default2@email.com')
         profile1 = Profile.objects.create(user=user1)
         post1 = Post.objects.create(title='test title', author=user1, body='this is a test body')
+        post2 = Post.objects.create(title='test title two', author=user2, body='this is ser2 test body')
         
     def post_like_count(self):
         post1 = Post.objects.get(title='test title')
@@ -40,4 +41,10 @@ class PostTestCase(TestCase):
         response = c.get("")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['object_list'].count(), 2)
+
+    def test_blog(self):
+        p = Post.objects.get(title='test title')
+        c = Client()
+        response = c.get(f"/article/{p.id}")
+        self.assertEqual(response.status_code, 200)
         
